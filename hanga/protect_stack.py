c45817e683fbc9fdc6ab0e88cdec7074613673ec
Sivadon Chaisiri <Sivadon.Chaisiri@2degrees.nz>
2020-01-26 13:53:00 +1300

    Add protect command

---
+++ hanga/protect_stack.py
@@ -0,0 +1,49 @@
+import sys
+
+import click
+import boto3
+import botocore
+
+from . import _session
+from . import hanga_constants as const
+from .describe_stack import _describe_stack
+
+@click.option('--name', '-n',
+                required=True,
+                help='Stack name')
+
+@click.option('--enable/--disable', '-e/-d',
+                help='Enable/disable the termination protection',
+                default=None,
+                is_flag=True)             
+
+@click.command(name='protect')
+def protect_stack(name, enable):
+    """
+    Enable a stack to be protected from termination
+    """    
+
+    click.secho('Current termination protection status: ', fg='green')  
+    status = str(_describe_stack(name, 
+            tuple([const.ENABLE_TERMINATION_PROTECTION]), 
+            False))
+
+    if enable is None:
+        click.secho(const.INF_NOTHING_TO_CHANGE, fg=const.FG_INF)
+    else:  
+        senable = str(enable)
+        
+        if (status in senable):
+            click.secho(const.INF_NOTHING_TO_CHANGE, fg=const.FG_INF)
+        else:
+            try:
+                _session.cf.update_termination_protection(StackName=name,
+                                                        EnableTerminationProtection=enable)
+                click.secho('The new termination protection status:', fg='green')
+                click.echo(enable)
+            except botocore.exceptions.ClientError:
+                click.secho(const.ERM_INVALID, bg=const.BG_ERROR, fg=const.FG_ERROR)
+                sys.exit(const.ERC_INVALID)
+            except:
+                click.secho(const.ERM_OTHERS, bg=const.BG_ERROR, fg=const.FG_ERROR)
+                sys.exit(const.ERC_OTHERS)      
