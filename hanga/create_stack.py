b2ece36eb238af0a1344c5ee468d0b0b1ceaeebd
Sivadon Chaisiri <Sivadon.Chaisiri@2degrees.nz>
2020-01-26 20:31:36 +1300

    Add create_stack command

--- hanga/create_stack.py
+++ hanga/create_stack.py
@@ -1,10 +1,13 @@
 import sys
+import os
 
 import click
 import boto3
+import botocore
 
 from . import _session
 from . import hanga_constants as const
+from . import hanga_util as util
 from . import upload_object
 
 @click.option('--name', '-n',
@@ -13,21 +16,22 @@ from . import upload_object
                 help='Stack name')
 
 @click.option('--template', '-t',
+                help='Stack template',
                 required=True,
-                type=click.STRING,
-                help='Stack template')
+                type=click.STRING)
 
 @click.option('--bucket', '-b',
-                required=True,
-                type=click.STRING,
-                help='S3 bucket prefix')
+                help='S3 bucket',
+                required=True)
+
+@click.option('--object-prefix', '-o',
+                help='S3 object prefix (i.e., directory)',
+                default='/')
 
 @click.option('--params', '-p',
-                type=click.STRING,
                 help='Parameter file')
 
 @click.option('--tags', '-t',
-                type=click.STRING,
                 help='Tag file')               
 
 @click.option('--upload', '-u',
@@ -35,19 +39,40 @@ from . import upload_object
                 is_flag=True)  
 
 @click.option('--default', '-d',
-                is_flag=True,
-                help='Default parameter and tag files')                               
+                help='Default parameter and tag files',
+                is_flag=True)                               
 
 @click.command(name='create')
-def create_stack(name, template, bucket, params, tags, upload, default):
+def create_stack(name, template, bucket, object_prefix, params, tags, upload, default):
     """
     Create a new stack
     """
+    object_prefix = util.reformS3Prefix(object_prefix)
+    try:
+        object_key = object_prefix + os.path.basename(open(template, 'r').name)
+    except FileNotFoundError:
+        click.secho(const.ERM_FILE_NOTFOUND, bg=const.BG_ERROR, fg=const.FG_ERROR)
+        sys.exit(const.ERC_FILE_NOTFOUND)      
 
-    pass
-    # try:
-    #     response = _session.s3.upload_object
+    if (upload):
+        click.secho('The template is being uploaded to the bucket.', fg=const.FG_INF)
+        upload_object._upload_object(bucket, object_key, os.path.abspath(template))
+        click.secho('Upload done...', fg=const.FG_INF)
+    
+    templateUrl = 'https://' + bucket + '.s3.amazonaws.com/' + object_key
 
-    # except botocore.exceptions.ClientError:
-    #     click.secho(const.ERM_STACK_NOTFOUND, bg=const.BG_ERROR, fg=const.FG_ERROR)
-    #     sys.exit(const.ERC_STACK_NOTFOUND)
+    try:
+        response = _session.cf.create_stack(StackName=name,
+                                            TemplateURL=templateUrl)
+    # except FileExistsError:
+    #     exit(1)
+    except botocore.exceptions.ClientError:
+        click.secho(const.ERM_INVALID_CREATE, bg=const.BG_ERROR, fg=const.FG_ERROR)
+        sys.exit(const.ERC_INVALID_CREATE)
+    except:
+        click.secho(const.ERM_OTHERS, bg=const.BG_ERROR, fg=const.FG_ERROR)
+        sys.exit(const.ERC_OTHERS)  
+    
+    stackId = response[const.STACK_ID]
+    click.secho('The following stack is being created:', fg=const.FG_INF)
+    click.echo(stackId)
